9ad98f62232e1b50aebc66912353d8a757d7cc2d
Nestor Vera <nestorvera@me.com>
2021-06-07 11:42:19 +0200

    Update default linters config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'standard',
   ],
-  env: {
-    'browser': true,
-  },
   rules: {
-    'comma-dangle': [
-      'error', {
-        'arrays': 'always-multiline',
-        'objects': 'always-multiline',
-        'imports': 'never',
-        'exports': 'never',
-        'functions': 'never',
-      },
-    ],
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
     'no-cond-assign': ['error', 'except-parens'],
+    'quote-props': ['warn', 'consistent-as-needed'],
+
+    'no-unused-expressions': 'off',
+    // 'babel/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
   },
 }
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,20 +5,26 @@ module.exports = {
   rules: {
     'at-rule-empty-line-before': [
       'always', {
-        'except': ['blockless-after-same-name-blockless', 'first-nested'],
-        'ignore': ['after-comment'],
-        'ignoreAtRules': ['apply', 'screen'],
+        except: ['blockless-after-blockless', 'blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment', 'inside-block'],
+        ignoreAtRules: ['tailwind', 'apply', 'screen', 'font-face', 'nest'],
       },
     ],
     'at-rule-no-unknown': [
       true, {
-        'ignoreAtRules': ['tailwind', 'variants', 'responsive', 'apply', 'screen'],
+        ignoreAtRules: ['tailwind', 'variants', 'responsive', 'apply', 'screen', 'layer'],
       },
     ],
     'property-no-unknown': [
       true, {
-        ignoreProperties: ['font-path']
+        ignoreProperties: ['font-path'],
       },
     ],
+    'selector-pseudo-element-no-unknown': [
+      true, {
+        ignorePseudoElements: ['range-track', 'range-thumb', 'range-lower', 'range-upper'],
+      },
+    ],
+    'selector-nested-pattern': '^&',
   },
 }
